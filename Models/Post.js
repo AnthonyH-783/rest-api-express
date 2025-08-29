9b7608967322745e21296597fb44f27985e50093
Anthony <anthonnhani@gmail.com>
2025-08-29 13:25:04 -0600

    Added a Post Database Schema

---
+++ Models/Post.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { type } = require("os");
+const { boolean } = require("webidl-conversions");
+
+const PostSchema = new mongoose.Schema({
+    userId:{
+        type: String,
+        required:true,
+    },
+    desc:{
+        type:String,
+        max:500,
+        default:''
+    },
+    img:{
+        type:String,
+        required:true
+    },
+    likes:{
+        type:Array,
+        default:[]
+    }
+
+
+},
+{timestamps : true}
+
+);
+
+module.exports = mongoose.model("Post", UserSchema);
